d0f2e660a71b721bcf4a2d37d81638f001f92216
deAd-otAku327 <almchech@mail.ru>
2025-09-09 14:05:46 +0300

    added socket.io traffic proxy support

--- go.mod
+++ go.mod
@@ -5,18 +5,21 @@ go 1.24
 toolchain go1.24.3
 
 require (
+	github.com/fasthttp/websocket v1.5.12
 	github.com/rs/zerolog v1.34.0
-	github.com/valyala/fasthttp v1.62.0
+	github.com/valyala/fasthttp v1.65.0
 	github.com/xakepp35/pkg v0.0.0-20250507224648-61faeaab3baf
 	golang.org/x/time v0.11.0
 	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
-	github.com/andybalholm/brotli v1.1.1 // indirect
+	github.com/andybalholm/brotli v1.2.0 // indirect
 	github.com/klauspost/compress v1.18.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/savsgio/gotils v0.0.0-20250408102913-196191ec6287 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/net v0.43.0 // indirect
+	golang.org/x/sys v0.35.0 // indirect
 )
--- handler/proxy.go
+++ handler/proxy.go
@@ -1,13 +1,28 @@
 package handler
 
 import (
+	"fmt"
+	"net/url"
 	"strings"
+	"sync"
+	"time"
 
+	"github.com/fasthttp/websocket"
 	"github.com/rs/zerolog/log"
 	"github.com/valyala/fasthttp"
 	"github.com/xakepp35/anygate/config"
 	"github.com/xakepp35/anygate/utils"
 )
+
+type connParams struct {
+	targetURL *url.URL
+	proxyFrom string
+	proxyTo   string
+
+	timeout              time.Duration
+	statusBadGateway     int
+	statusGatewayTimeout int
+}
 
 // 🚀 NewProxy — проксирует с сохранением raw query и без мутации исходного ctx.Request
 func NewProxy(from, to string, cfg config.Proxy) fasthttp.RequestHandler {
@@ -22,10 +37,19 @@ func NewProxy(from, to string, cfg config.Proxy) fasthttp.RequestHandler {
 	}
 
 	pathBuilder := utils.NewPathBuilder(to, len(from), cfg.RouteLenHint)
+
 	client := NewClient(cfg)
+	connParams := connParams{
+		proxyFrom: from,
+		proxyTo:   to,
+
+		timeout:              cfg.Timeout,
+		statusBadGateway:     cfg.StatusBadGateway,
+		statusGatewayTimeout: cfg.StatusGatewayTimeout,
+	}
 
 	return func(ctx *fasthttp.RequestCtx) {
-		// 1) Собираем базовую цель (host+scheme+path) через твой builder
+		// 1) Собираем базовую цель (scheme+host+path) через builder
 		target := pathBuilder.Build(ctx.Path())
 
 		// 2) Приклеиваем СЫРОЙ query из входящего запроса (без переэнкодинга)
@@ -37,53 +61,150 @@ func NewProxy(from, to string, cfg config.Proxy) fasthttp.RequestHandler {
 			}
 		}
 
-		// 3) Готовим отдельный запрос (не трогаем ctx.Request)
-		req := fasthttp.AcquireRequest()
-		resp := fasthttp.AcquireResponse()
-		defer func() {
-			fasthttp.ReleaseRequest(req)
-			fasthttp.ReleaseResponse(resp)
-		}()
-
-		ctx.Request.CopyTo(req)     // копируем метод/заголовки/тело
-		req.SetRequestURI(target)   // абсолютный URI с нашим query
-		req.SetTimeout(cfg.Timeout) // если задан
-
-		// Проставим корректный Host для апстрима (на всякий случай)
-		if h := req.URI().Host(); len(h) > 0 {
-			req.Header.SetHostBytes(h)
-		}
-
-		log.Info().
-			Str("from", from).
-			Str("to", to).
-			Str("method", string(req.Header.Method())).
-			Str("target", target).
-			Bytes("raw_query_in", ctx.URI().QueryString()).
-			Msg("proxy -> upstream")
-
-		// 4) Шлём запрос и кладём ответ прямо в ctx.Response
-		if err := client.Do(req, resp); err != nil {
-			if err == fasthttp.ErrTimeout {
-				ctx.SetStatusCode(cfg.StatusGatewayTimeout)
-				ctx.SetBodyString(`{"error":"timeout"}`)
-				log.Error().Err(err).Str("to", to).Str("from", from).Msg("timeout")
-				return
-			}
-			ctx.SetStatusCode(cfg.StatusBadGateway)
-			ctx.SetBodyString(`{"error":"` + err.Error() + `"}`)
-			log.Error().Err(err).Str("to", to).Str("from", from).Msg("gateway")
+		targetURL, err := url.Parse(target)
+		if err != nil {
+			ctx.Error("Internal server error", fasthttp.StatusInternalServerError)
+			log.Error().
+				Err(err).
+				Str("rawURL", target).
+				Msg("URL parsing failed")
 			return
 		}
 
-		// Копируем ответ
-		ctx.Response.SetStatusCode(resp.StatusCode())
-		resp.Header.CopyTo(&ctx.Response.Header)
-		ctx.Response.SetBodyRaw(resp.Body()) // zero-copy set
+		connParams.targetURL = targetURL
 
-		log.Info().
-			Str("to", to).
-			Int("status", resp.StatusCode()).
-			Msg("Proxy success")
+		if string(ctx.Method()) == "GET" && websocket.FastHTTPIsWebSocketUpgrade(ctx) {
+			handleWebSocketConnection(ctx, &connParams)
+			return
+		}
+
+		handleHTTPConnection(ctx, client, &connParams)
 	}
+}
+
+func handleHTTPConnection(ctx *fasthttp.RequestCtx, client *fasthttp.Client, cp *connParams) {
+	// 3) Готовим отдельный запрос (не трогаем ctx.Request)
+	req := fasthttp.AcquireRequest()
+	resp := fasthttp.AcquireResponse()
+	defer func() {
+		fasthttp.ReleaseRequest(req)
+		fasthttp.ReleaseResponse(resp)
+	}()
+
+	ctx.Request.CopyTo(req)                  // копируем метод/заголовки/тело
+	req.SetRequestURI(cp.targetURL.String()) // абсолютный URI с нашим query
+	req.SetTimeout(cp.timeout)               // если задан
+
+	// Проставим корректный Host для апстрима (на всякий случай)
+	if h := req.URI().Host(); len(h) > 0 {
+		req.Header.SetHostBytes(h)
+	}
+
+	log.Info().
+		Str("from", cp.proxyFrom).
+		Str("to", cp.proxyTo).
+		Str("method", string(req.Header.Method())).
+		Str("target", cp.targetURL.String()).
+		Bytes("raw_query_in", ctx.URI().QueryString()).
+		Msg("proxy -> upstream")
+
+	// 4) Шлём запрос и кладём ответ прямо в ctx.Response
+	if err := client.Do(req, resp); err != nil {
+		if err == fasthttp.ErrTimeout {
+			ctx.SetStatusCode(cp.statusGatewayTimeout)
+			ctx.SetBodyString(`{"error":"timeout"}`)
+			log.Error().Err(err).Str("from", cp.proxyFrom).Str("to", cp.proxyTo).Msg("timeout")
+			return
+		}
+		ctx.SetStatusCode(cp.statusBadGateway)
+		ctx.SetBodyString(`{"error":"` + err.Error() + `"}`)
+		log.Error().Err(err).Str("from", cp.proxyFrom).Str("to", cp.proxyTo).Msg("gateway")
+		return
+	}
+
+	// Копируем ответ
+	ctx.Response.SetStatusCode(resp.StatusCode())
+	resp.Header.CopyTo(&ctx.Response.Header)
+	ctx.Response.SetBodyRaw(resp.Body()) // zero-copy set
+
+	log.Info().
+		Str("to", cp.proxyTo).
+		Int("status", resp.StatusCode()).
+		Msg("Proxy success")
+}
+
+func handleWebSocketConnection(ctx *fasthttp.RequestCtx, cp *connParams) {
+	cp.targetURL.Scheme = "ws"
+	fmt.Println("socket on " + cp.targetURL.String())
+
+	serverConn, _, err := websocket.DefaultDialer.Dial(
+		cp.targetURL.String(),
+		nil,
+	)
+	if err != nil {
+		log.Printf("Error connecting to server WebSocket on %s: %v", cp.targetURL, err)
+		ctx.Error("Failed to connect to server", fasthttp.StatusBadGateway)
+		return
+	}
+
+	connUpgrader := websocket.FastHTTPUpgrader{
+		CheckOrigin: func(ctx *fasthttp.RequestCtx) bool {
+			return true
+		},
+	}
+
+	// Обновляем соединение клиента до WebSocket
+	err = connUpgrader.Upgrade(ctx, func(clientConn *websocket.Conn) {
+
+		defer func() {
+			serverConn.Close()
+			clientConn.Close()
+		}()
+
+		var wg sync.WaitGroup
+		wg.Add(2)
+
+		go func() {
+			defer wg.Done()
+			forwardWebSocket(ctx, clientConn, serverConn, "client->server")
+		}()
+
+		go func() {
+			defer wg.Done()
+			forwardWebSocket(ctx, serverConn, clientConn, "server->client")
+		}()
+
+		wg.Wait()
+		fmt.Printf("CALLBACK END for %s", clientConn.RemoteAddr())
+	})
+
+	if err != nil {
+		fmt.Printf("WebSocket upgrade error: %v", err)
+	}
+
+	fmt.Printf("HANDLER END for %s", ctx.RemoteAddr())
+}
+
+func forwardWebSocket(ctx *fasthttp.RequestCtx, src, dst *websocket.Conn, direction string) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			messageType, message, err := src.ReadMessage()
+			fmt.Println("re ", string(message))
+			if err != nil {
+				fmt.Printf("WebSocket read error (%s): %v", direction, err)
+				return
+			}
+
+			err = dst.WriteMessage(messageType, message)
+			fmt.Println("wr ", string(message))
+			if err != nil {
+				fmt.Printf("WebSocket write error (%s): %v", direction, err)
+				return
+			}
+		}
+	}
+
 }
